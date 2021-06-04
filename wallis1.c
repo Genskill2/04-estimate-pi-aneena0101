cbb51baa8e4e85605249f408df0f78f6b09d8f2c
aneena0101 <85095161+aneena0101@users.noreply.github.com>
2021-06-04 12:52:40 +0530

    Create wallis1.c

---
+++ wallis1.c
@@ -0,0 +1,32 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+float wallis_pi(int l)
+{ int k=1;
+ 
+    if(l>=1)
+    {     k*=((4*l*l)/((4*l*l)-1);
+              wallis_pi(l-1);
+              }
+              return (2*k);
+              }
+int main(void) {
+  float pi;
+  for (int i=1; i<5; i++) {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) > 0.15)) {
+      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
+      abort();
+    }
+  }
+
+  for (int i=500; i<3000; i++) {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) < 0.01)) {
+      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
+      abort();
+    }
+  }
+}
